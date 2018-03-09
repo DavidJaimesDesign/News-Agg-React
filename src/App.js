462ae55e71db900a805239a46369e5b3b3365632
David Jaimes <davidjaimesdesign@gmail.com>
2018-03-08 18:45:08 -0800

    Got rid of those god awful spaces

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+//Hacker news Path variables
 const DEFAULT_QUERY = '';
 const DEFAULT_PAGE = 0;
 const DEFAULT_HPP = '100';
@@ -9,74 +10,75 @@ const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
 const PARAM_HPP = 'hitsPerPage=';
 
+//Medium path variables
+
+//Reddit path variables
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      results: null,
-      query: DEFAULT_QUERY,
-      searchKey: '',
-    };
-    this.setSearchTopstories = this.setSearchTopstories.bind(this);
-    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
-    this.needsToSearchTopstories = this.needsToSearchTopstories.bind(this);
-  }
-  setSearchTopstories(result) {
-    const { hits, page } = result;
-    const { searchKey } = this.state;
-    const oldHits = page === 0 ? [] : this.state.results[searchKey].hits;
-    const updatedHits = [ ...oldHits, ...hits ];
-    this.setState({
-      results: { ...this.state.results, [searchKey]: { hits: updatedHits, page }}
-    });
-  }
+ 	constructor(props) {
+ 		super(props);
+ 		this.state = {
+			results: null,
+			query: DEFAULT_QUERY,
+			searchKey: '',
+ 		};
+		this.setSearchTopstories = this.setSearchTopstories.bind(this);
+		this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
+ 	}
 
-  fetchSearchTopstories(query, page) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-    .then(response => response.json())
-    .then(result => this.setSearchTopstories(result));
-  }
+ 	setSearchTopstories(result) {
+		const { hits, page } = result;
+		const { searchKey } = this.state;
+		const oldHits = page === 0 ? [] : this.state.results[searchKey].hits;
+		const updatedHits = [ ...oldHits, ...hits ];
+		this.setState({
+			results: { ...this.state.results, [searchKey]: { hits: updatedHits, page }}
+		});
+ 	}
 
-  componentDidMount() {
-    const { query } = this.state;
-    this.setState({ searchKey: query });
-    this.fetchSearchTopstories(query, DEFAULT_PAGE);
-  }
+	fetchSearchTopstories(query, page) {
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+		.then(response => response.json())
+		.then(result => this.setSearchTopstories(result));
+	}
 
-  needsToSearchTopstories(query) {
-    return !this.state.results[query];
-  }
+	componentDidMount() {
+		const { query } = this.state;
+		this.setState({ searchKey: query });
+		this.fetchSearchTopstories(query, DEFAULT_PAGE);
+	}
 
-  render() {
-  const { query, results, searchKey } = this.state;
-  const page = (results && results[searchKey] && results[searchKey].page) || 0;
-  const list = (results && results[searchKey] && results[searchKey].hits) || [];
-  return (
-  <div className="page">
-    <Table list={list} />
-  </div>
-  );
-  }
+	render() {
+		console.log(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+		const { query, results, searchKey } = this.state;
+ 		const page = (results && results[searchKey] && results[searchKey].page) || 0;
+ 		const list = (results && results[searchKey] && results[searchKey].hits) || [];
+ 		return (
+ 			<div className="page">
+				<Table list={list} />
+ 			</div>
+ 		);
+ 	}
 }
 
 const Table = ({ list }) =>
-  <div className="table">
-    { list.map((item) =>
-      <div key={item.objectID} className="table-row">
-        <span style={{ width: '40%' }}>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span style={{ width: '30%' }}>
-          {item.author}
-        </span>
-        <span style={{ width: '15%' }}>
-          {item.num_comments}
-        </span>
-        <span style={{ width: '15%' }}>
-            {item.points}
-          </span>
-      </div>
-    )}    
-  </div>
+ 	<div className="table">
+		{ list.map((item) =>
+			<div key={item.objectID} className="table-row">
+				<span style={{ width: '40%' }}>
+          			<a href={item.url}>{item.title}</a>
+        		</span>
+ 				<span style={{ width: '30%' }}>
+  					{item.author}
+				</span>
+				<span style={{ width: '15%' }}>
+  					{item.num_comments}
+				</span>
+				<span style={{ width: '15%' }}>
+					{item.points}
+				</span>
+ 			</div>
+ 		)}    
+ 	</div>
 
 export default App;
